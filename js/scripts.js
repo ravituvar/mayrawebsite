2386b9f0a7f6acaae6a3b9d73c164ff8604b16a0
Ravi Tuvar <ravituvar@yahoo.com>
2017-12-30 17:37:16 +0530

    a
    a

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(window).load(function() {
 
 $(document).ready(function(){
   $("#counter").countdown({
-  until: new Date(2016, 12 - 1, 15),
+  until: new Date(2018, 12 - 1, 15),
   format: 'dHMS'
   });
 
